5720a1fee388be3045fccd3ee59897663e8f7ba9
Jonas64 <72814464+Jonas64@users.noreply.github.com>
2024-03-24 11:48:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ folder_directory = ""
 groups = []
 advanced_btn_text = ["Avanserte alternativer v", "Avanserte alternativer ^"]
 separated = []
+together = []
 
 
 # Functions
@@ -85,6 +86,35 @@ def change_num_groups_num_ppl(): # Bytte mellom å velg antall folk i hver grupp
         num_groups_num_ppl_lbl3.config(foreground=disabled_color)
         r.update_idletasks()
 
+def check_name(name, group, names, group_size): # Sjekke om en person kan være i en gruppe
+    name_sep = [] # Navn som personen ikke kan være med
+    for sep in separated:
+        if name in sep:
+            if name == sep[0]:
+                name_sep.append(sep[1])
+            else:
+                name_sep.append(sep[0])
+    name_t = [] # Navn som personen må være med
+    for t in together:
+        if name in t:
+            if name == t[0]:
+                name_t.append(t[1])
+            else:
+                name_t.append(t[0])
+    
+    for sep in name_sep:
+        if sep in group: # Hvis et av navnene som personen ikke kan være med er i gruppa
+            return False
+    for t in name_t:
+        if t in names:
+            return False
+        else:
+            if t not in group: # Hvis et av navnene som personen må være med ikke er i gruppa
+                return False
+    if len(group) == group_size:
+        return False
+    return True
+
 def generate_groups():
     global groups, groups_listboxes, groups_listboxes_frame
     if not num_groups_num_ppl.get(): # Hvis brukeren valgte antall grupper
@@ -103,48 +133,82 @@ def generate_groups():
         messagebox.showerror("Feil", f"Skriv minst {group_size}(antall per gruppe) navn.")
         return
     else:
-        names_shuffle = names.copy()
-        separated_bool = False # Anta at de ikke er separert
-        while not separated_bool:
-            random.shuffle(names_shuffle) # Tilfeldig rekkefølge på navn liste
-            groups = []
-            if len(names)%num_groups_ == 0: # Hvis antall personer kan deles på antall grupper
-                start = 0
-                for i in range(num_groups_): # Velg en del av names_shuffle og lag en gruppe av de
-                    end = min(start+group_size, len(names_shuffle))
-                    group = names_shuffle[start:end]
-                    groups.append(group)
-                    start = end
-            else: # Hvis antall personer og antall grupper ikke går opp
-                start = 0
-                for i in range(num_groups_):
-                    end = min(start+group_size, len(names_shuffle))
-                    group = names_shuffle[start:end]
-                    groups.append(group)
-                    start = end
-                not_in_group = [] # Legge til navnene som ikke ble inkludert (blir ujevne grupper)
-                in_group = []
-                for g in groups: # For group in groups:
-                    for n in g: # For name in group:
-                        in_group.append(n)
-                for n in names:
-                    if n not in in_group:
-                        not_in_group.append(n)
-                i = 0
-                for n in not_in_group: # Legge til navnene som ikke ble med
-                    i = i%num_groups_
-                    groups[i].append(n)
-                    i += 1
-            if separated != []: # Hvis brukeren vil separere to personer
-                for group in groups:
-                    if separated[0] in group and separated[1] in group: # Hvis begge er i samme gruppe
-                        separated_bool = False
+        together_dict = {} # Alle navnene som skal være sammen med noen og hvem de skal være sammen med
+        for t in together:
+            for n in t:
+                together_dict[n] = []
+        for name in together_dict:
+            for t in together:
+                if name in t:
+                    if name == t[0]:
+                        together_dict[name].append(t[1])
                     else:
-                        separated_bool = True
-            else:
-                separated_bool = True
-            print(separated)
+                        together_dict[name].append(t[0])
+        
+        separated_dict = {} # Alle navnene som skal være separert med noen og hvem de skal være separert med
+        for sep in separated:
+            for n in sep:
+                separated_dict[n] = []
+        for name in separated_dict:
+            for sep in separated:
+                if name in sep:
+                    if name == sep[0]:
+                        separated_dict[name].append(sep[1])
+                    else:
+                        separated_dict[name].append(sep[0])
+        
+        names_shuffle = [1]
 
+        outer_loop_count = 0
+        while len(names_shuffle) > 0 and outer_loop_count < 100: # Hvis det ikke gikk å generere grupper, lag ny names_shuffle og prøv igjen
+            names_shuffle = names.copy() # Lag ny names_shuffle hvis det ikke gikk
+            random.shuffle(names_shuffle)
+
+            groups = []
+            for i in range(num_groups_):
+                groups.append([])
+
+            i = 0
+            loop_count = 0
+            while len(names_shuffle) > 0 and loop_count < 100: # Generere grupper med names_shuffle
+                to_remove = []
+                for name in names_shuffle: # Sett folk inn i grupper
+                    separated_ok = True
+                    if name in separated_dict:
+                        for sep_name in separated_dict[name]:
+                            if sep_name in groups[i]:
+                                separated_ok = False
+
+                    together_ok = True
+                    if name in together_dict:
+                        for t_name in together_dict[name]:
+                            groups_empty = True
+                            for g_i, group in enumerate(groups):
+                                for t in together_dict[name]:
+                                    if t in group and g_i != i: # Hvis den som navnet må være med er i en annen gruppe
+                                        together_ok = False
+                                if group != []:
+                                    groups_empty = False
+                            
+                            if t_name not in groups[i] and not groups_empty: # Hvis navnet ikke er i gruppa og gruppene ikke er tomme
+                                together_ok = False
+
+                    if separated_ok and together_ok: # Sjekke om navnet går i gruppa
+                        groups[i].append(name)
+                        to_remove.append(name)
+                        i += 1
+                        i %= num_groups_
+                
+                if len(to_remove) == 0:
+                    i += 1
+                    i %= num_groups_
+
+                for n in to_remove:
+                    if n in names_shuffle:
+                        names_shuffle.remove(n)
+                loop_count += 1
+            outer_loop_count += 1
+        
     groups_listboxes_frame.destroy() # Destroy frame og alle listboxes i den (fjerne forrige grupper som ble generert)
     groups_listboxes = []
     groups_listboxes_frame = ttk.Frame(r)
@@ -153,7 +217,7 @@ def generate_groups():
     for g in groups:
         groups_listboxes.append(tk.Listbox(groups_listboxes_frame, width=int((r.winfo_width()//len(groups))//7.2))) # Legg til listbox
         j = 0
-        for n in g: # LEgg til alle navnene i riktig listbox
+        for n in g: # Legg til alle navnene i riktig listbox
             groups_listboxes[i].insert(j, n)
             j += 1
         i += 1
@@ -202,12 +266,59 @@ def set_not_in_group(): # Velg to personer som ikke skal være i samme gruppe
     selected_names = []
     for i in selected_names_i:
         selected_names.append(names[i])
+
+    if selected_names in separated or [selected_names[1], selected_names[0]] in separated: # Hvis navnene allerede er separert
+        messagebox.showerror("Feil", f"Navnene er allerede separert. ({''.join(selected_names)})")
+        return
     
-    separated = selected_names
+    if selected_names in together or [selected_names[1], selected_names[0]] in together: # Hvis navnene allerede er sammen
+        messagebox.showerror("Feil", f"Du kan ikke både separere og sette sammen to navn.")
+        return
+    
+    separated.append(selected_names)
+    sep_string = ""
+    for sep in separated: # Konverter separated lista til en string som skal vises på en Label
+        sep_string += " | ".join(sep)
+        sep_string += ",  "
+    sep_string = sep_string[:-3] # Fjern siste komma siden det ikke er noe etter det
+    not_in_group_lbl.config(text=sep_string) # Oppdater Label
+
+def set_in_group():
+    global together
+    selected_names_i = []
+    for i in name_list.curselection():
+        selected_names_i.append(i)
+
+    if len(selected_names_i) != 2:
+        messagebox.showerror("Feil", f"Velg to navn. (du har valgt {len(selected_names_i)})")
+        return
+
+    selected_names = []
+    for i in selected_names_i:
+        selected_names.append(names[i])
+
+    if selected_names in together or [selected_names[1], selected_names[0]] in together: # Hvis navnene allerede er separert
+        messagebox.showerror("Feil", f"Navnene er allerede sammen. ({''.join(selected_names)})")
+        return
+    
+    if selected_names in separated or [selected_names[1], selected_names[0]] in separated: # Hvis navnene allerede er separert
+        messagebox.showerror("Feil", f"Du kan ikke både separere og sette sammen to navn.")
+        return
+    
+    together.append(selected_names)
+    together_string = ""
+    for sep in together: # Konverter separated lista til en string som skal vises på en Label
+        together_string += " - ".join(sep)
+        together_string += ",  "
+    together_string = together_string[:-3] # Fjern siste komma siden det ikke er noe etter det
+    set_in_group_lbl.config(text=together_string) # Oppdater Label
 
 def reset_advanced(): # Reset alle avanserte alternativer
-    global separated
+    global separated, together
+    together = []
     separated = []
+    not_in_group_lbl.config(text="")
+    set_in_group_lbl.config(text="")
 
 
 
@@ -221,6 +332,8 @@ groups_listboxes_frame = ttk.Frame(r)
 choose_directory_frame = ttk.Frame(r)
 directory_lbl_frame = ttk.Frame(r)
 advanced_options_frame = ttk.Frame(r)
+not_in_group_frame = ttk.Frame(advanced_options_frame)
+set_in_group_frame = ttk.Frame(advanced_options_frame)
 
 # Widgets
 name_list = tk.Listbox(r, selectmode=tk.EXTENDED, font=("TkDefaultFont", 14))
@@ -243,7 +356,10 @@ num_ppl_per_group_lbl = ttk.Label(num_ppl_per_group_frame, text="Antall per grup
 num_ppl_per_group_spinbox = ttk.Spinbox(num_ppl_per_group_frame, textvariable=num_ppl_per_group, from_=2, to=999, increment=1, state=tk.DISABLED)
 
 advanced_options_btn = ttk.Button(r, text=advanced_btn_text[0], command=open_advanced)
-not_in_group_btn = ttk.Button(advanced_options_frame, text="Separer", command=set_not_in_group)
+not_in_group_btn = ttk.Button(not_in_group_frame, text="Separer", command=set_not_in_group)
+not_in_group_lbl = ttk.Label(not_in_group_frame, text="")
+set_in_group_btn = ttk.Button(set_in_group_frame, text="Sett sammen", command=set_in_group)
+set_in_group_lbl = ttk.Label(set_in_group_frame, text="")
 reset_advanced_btn = ttk.Button(advanced_options_frame, text="Reset avanserte alternativer", command=reset_advanced)
 
 generate_groups_btn = ttk.Button(r, text="Generer grupper", command=generate_groups)
@@ -258,15 +374,15 @@ export_btn = ttk.Button(r, text="Eksporter", command=export_groups)
 
 
 # Pack alle tingene
-add_name_frame.pack(side=tk.TOP, pady=20)
+add_name_frame.pack(side=tk.TOP, pady=10)
 name_list.pack(fill="x", padx=30)
 remove_name_btn.pack(side=tk.LEFT, padx=5)
 import_names_btn.pack(side=tk.RIGHT, padx=5)
-remove_name_import_name_frame.pack(pady=10)
+remove_name_import_name_frame.pack(pady=5)
 add_name_entry.pack(side=tk.LEFT, padx=10)
 add_name_btn.pack(side=tk.RIGHT, padx=10)
 
-num_groups_num_ppl_check_frame.pack(pady=10)
+num_groups_num_ppl_check_frame.pack(pady=5)
 num_groups_num_ppl_check.pack(side=tk.RIGHT)
 num_groups_num_ppl_lbl1.pack(side=tk.LEFT)
 num_groups_num_ppl_lbl2.pack(side=tk.LEFT)
@@ -280,7 +396,10 @@ num_ppl_per_group_frame.pack(pady=5)
 num_ppl_per_group_lbl.pack(side=tk.LEFT, padx=10)
 num_ppl_per_group_spinbox.pack(side=tk.RIGHT, padx=10)
 
-
+not_in_group_btn.pack()
+not_in_group_lbl.pack()
+set_in_group_btn.pack()
+set_in_group_lbl.pack()
 advanced_options_btn.pack(pady=5)
 advanced_options_frame.pack()
 
@@ -294,6 +413,6 @@ choose_directory_frame.pack()
 directory_lbl.pack(pady=5, side=tk.LEFT)
 file_name_directory_lbl.pack(side=tk.RIGHT)
 directory_lbl_frame.pack()
-export_btn.pack()
+export_btn.pack(pady=5)
 
 r.mainloop()
